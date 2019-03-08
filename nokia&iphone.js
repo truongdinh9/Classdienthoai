dfd0de8dcb627ea7d21cde060157c6c6ea4d8ed6
dinh.truon <dinh.kttn.neu@gmail.com>
2019-03-08 23:16:03 +0700

    rút ngắn code

--- nokia&iphone.js
+++ nokia&iphone.js
@@ -3,6 +3,7 @@ function phones(onoff, battery) {
     this.battery = battery
     this.inbox = []
     this.outbox = []
+    this.hienthi=""
 }
 // const log=console.log;
 let Nokia = new phones("off", 5);
@@ -11,105 +12,108 @@ let Iphone = new phones("off", 5);
 /**
  * @return {boolean}
  */
-function Nokiahoatdong() { return (Nokia.onoff === "on" && Nokia.battery > 0)}
+function hoatdong(aphone) { return (aphone.onoff === "on" && aphone.battery > 0)}
 
 /**
  * @return {boolean}
  */
-function Iphonehoatdong() {return (Iphone.onoff === "on" && Iphone.battery > 0)}
 
-function turnonNokia() {
-    Nokia.onoff = "on"
+
+function turnon(aphone) {
+    aphone.onoff = "on"
 }
 
-function turnoffNokia() {
-    Nokia.onoff = "off"
+function turnoff(aphone) {
+    aphone.onoff = "off"
 }
 
-function ChargeNokia() {
-    if (Nokia.battery !== 10) {
-        Nokia.battery += 1
+function Charge(aphone) {
+    if (aphone.battery !== 10) {
+        aphone.battery += 1
     }
 }
 
-function BateryNokia() {
-    alert(Nokia.battery);
-    Nokia.battery--;
+function Batery(aphone) {
+    alert(aphone.battery);
+    aphone.battery--;
 }
 
-function sendNokia() {
-    if (Nokiahoatdong()) {
-        Nokia.outbox.unshift(document.getElementById("nokia").value);
-        Iphone.inbox.unshift(document.getElementById("nokia").value);
-        Nokia.battery--
+function send(aphone,bphone) {
+    if (hoatdong(aphone)) {
+        Nokia.hienthi=document.getElementById("nokia").value
+        Iphone.hienthi=document.getElementById("iphone").value
+        aphone.outbox.unshift(aphone.hienthi)
+        bphone.inbox.unshift(aphone.hienthi);
+        aphone.battery--
         nokia.value=""
-    }
-}
-
-function inboxNokia() {
-    if (Nokiahoatdong()) {
-        let i = prompt("Bạn có:" + Nokia.inbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
-        if (parseInt(i) == i && i != 0) {
-            document.getElementById("nokia").value = Nokia.inbox[i - 1]
-        }
-        battery--
-    }
-}
-
-function outboxNokia() {
-    if (Nokiahoatdong()) {
-        let i = prompt("Bạn có:" + Nokia.outbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
-        if (parseInt(i) == i && i != 0) {
-            document.getElementById("nokia").value = Nokia.outbox[i - 1]
-        }
-        Nokia.battery--
-    }
-}
-
-function turnonIphone() {
-    Iphone.onoff = "on"
-}
-
-function turnoffIphone() {
-    Iphone.onoff = "off"
-}
-
-function ChargeIphone() {
-    if (Iphone.battery !== 10) {
-        Iphone.battery += 1
-    }
-}
-
-function BateryIphone() {
-    alert(Iphone.battery)
-    Iphone.battery--
-}
-
-function sendIphone() {
-    if (Iphonehoatdong()) {
-        Iphone.outbox.unshift(document.getElementById("iphone").value);
-        Nokia.inbox.unshift(document.getElementById("iphone").value);
         iphone.value=""
-        Iphone.battery--
     }
 }
 
-function inboxIphone() {
-    if (Iphonehoatdong()) {
-        let i = prompt("Bạn có:" + Iphone.inbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
-        if (parseInt(i) == i && i!= 0) {
-            document.getElementById("iphone").value = Iphone.inbox[i - 1]
+function inbox(aphone) {
+    if (hoatdong(aphone)) {
+        let i = prompt("Bạn có:" + aphone.inbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
+        if (parseInt(i) == i && i != 0) {
+          if(aphone==Nokia)  {nokia.value = aphone.inbox[i - 1]}else {iphone.value=aphone.inbox[i - 1]}
         }
-        Iphone.battery--
+        aphone.battery--
     }
 }
 
-function outboxIphone() {
-    if (Iphonehoatdong()) {
-        let i = prompt("Bạn có:" + Iphone.outbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
-        if (parseInt(i) == i && i!= 0) {
-            document.getElementById("iphone").value = Iphone.inbox[i - 1]
+function outbox(aphone) {
+    if (hoatdong(aphone)) {
+        let i = prompt("Bạn có:" + aphone.outbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
+        if (parseInt(i) == i && i != 0) {
+            if(aphone==Nokia)  {nokia.value = aphone.outbox[i - 1]}else {iphone.value=aphone.outbox[i - 1]}
         }
-        Iphone.battery--
+        aphone.battery--
     }
 }
+
+// function turnonIphone() {
+//     Iphone.onoff = "on"
+// }
+//
+// function turnoffIphone() {
+//     Iphone.onoff = "off"
+// }
+//
+// function ChargeIphone() {
+//     if (Iphone.battery !== 10) {
+//         Iphone.battery += 1
+//     }
+// }
+//
+// function BateryIphone() {
+//     alert(Iphone.battery)
+//     Iphone.battery--
+// }
+//
+// function sendIphone() {
+//     if (Iphonehoatdong()) {
+//         Iphone.outbox.unshift(document.getElementById("iphone").value);
+//         Nokia.inbox.unshift(document.getElementById("iphone").value);
+//         iphone.value=""
+//         Iphone.battery--
+//     }
+// }
+//
+// function inboxIphone() {
+//     if (Iphonehoatdong()) {
+//         let i = prompt("Bạn có:" + Iphone.inbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
+//         if (parseInt(i) == i && i!= 0) {
+//             document.getElementById("iphone").value = Iphone.inbox[i - 1]
+//         }
+//         Iphone.battery--
+//     }
+// }
+//
+// function outboxIphone() {
+//     if (Iphonehoatdong()) {
+//         let i = prompt("Bạn có:" + Iphone.outbox.length + "tin nhắn. Vui lòng chọn thứ tự tin nhắn bạn muốn đọc")
+//         if (parseInt(i) == i && i!= 0) {
+//             document.getElementById("iphone").value = Iphone.inbox[i - 1]
+//         }
+//         Iphone.battery--
+//     }
+// }
